5c0427c752ccc6c74f37ef3eab4f44c8c58a62a4
esm234 <97116875+esm234@users.noreply.github.com>
2025-08-22 05:11:25 +0300

    Update Homenew.tsx

--- src/pages/Homenew.tsx
+++ src/pages/Homenew.tsx
@@ -412,7 +412,7 @@ return (
                       <img 
                         src={member.image} 
                         alt={member.name}
-                        className="w-full h-full object-cover"
+                        className="w-64 h-40 object-cover"
                       />
                     </div>
                   </div>
